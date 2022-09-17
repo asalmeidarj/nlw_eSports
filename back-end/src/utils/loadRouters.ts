15d2250c7977a1903ac81c47cc66a0d633b38121
asalmeidarj <asalmeidarj@gmail.com>
2022-09-17 19:55:44 -0300

    função para carregamento das rotas no express

---
+++ back-end/src/utils/loadRouters.ts
@@ -0,0 +1,8 @@
+import { routers } from "../routers/routers";
+import { Express } from "express";
+
+export default function loadRouters(app: Express): void {
+    for(const router of routers){
+        app.use('/', router)
+    }
+}
