6de9e25d6ee47a1bdaaea3c38990c6c71f3cc349
asalmeidarj <asalmeidarj@gmail.com>
2022-09-18 20:48:51 -0300

    Inserindo cors no express

--- back-end/src/config/customExpress.ts
+++ back-end/src/config/customExpress.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { Express } from 'express'
 import loadRouters from '../utils/routers/loadRouters'
+import cors from 'cors'
 
 //Configuração do Express
 const app: Express = express()
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
