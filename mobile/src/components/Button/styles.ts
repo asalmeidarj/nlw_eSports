1d5aeac1305e2dd70117efdd10970b6d33c64c5a
asalmeidarj <asalmeidarj@gmail.com>
2022-10-01 23:21:28 -0300

    ajustes

--- mobile/src/components/Button/styles.ts
+++ mobile/src/components/Button/styles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     display: 'flex',
-    flexDirection: 'row',
     justifyContent: 'space-around',
   },
 });
